e2c0fd8829a26d6e173827a8b7bf9f5f42bbf385
moallelite <mouhamedn347@gmail.com>
2024-12-05 21:41:35 +0000

    7.1 Amazon web service authentification configuration

--- src/app/services/aws-authentication.service.ts
+++ src/app/services/aws-authentication.service.ts
@@ -1,18 +1,52 @@
 import { Injectable } from '@angular/core';
-import AWS, {S3Client} from "@aws-sdk/client-s3";
-@Injectable({
-  providedIn: 'root'
-})
-export class AwsAuthenticationService {
+import aws from 'aws-sdk';
+import crypto, { randomBytes } from 'crypto';  import { promisify } from 'util';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {environment} from '../../environments/environment';
+@Injectable({ providedIn: 'root'})
+export class AwsService {
+  randomBytes = promisify(crypto.randomBytes);
+  bucketName: string=environment.bucketName;
 
-  constructor() { }
-  region:string='';
+  constructor(private http:HttpClient) { }
 
-  public  getAuthentication(){
-    const client = new  S3Client(
-      {
-         region:this.region,
-        
-      })
+  // params for S3 auth Import from our environnement
+  region :string = environment.region;
+  accessKeyId :string = environment.accessKeyId;
+  secretAccessKey :string = environment.secretAccessKey;
+  minuteExpire:number = 60;
+
+  //Ulpoad an image on AWS S3
+  public async generateUploadUrl( ){
+    //GET IAM user authentication by accesKeyId & secretKey
+    const s3 = new aws.S3({
+      region: this.region,
+      accessKeyId: this.accessKeyId,
+      secretAccessKey: this.secretAccessKey
+    });
+
+    // image name encryption
+    const [rawByte] = await Promise.all([randomBytes(16)]);
+    const imageName = rawByte.toString('hex');
+    const params = ({
+      Bucket:this.bucketName,
+      Key:imageName,
+      Expire: this.minuteExpire  });
+    const uploadUrl:any= await s3.getSignedUrlPromise(   'putObject',params  );    return uploadUrl;   }
+
+  // get secure URL from our server
+  public async getSecureUrl(){
+    const url = await this.generateUploadUrl();
+    console.log(url);
+    console.log(`type of : ${typeof url}`);
+    return this.http.get(url);
+  }
+
+  //POST the image directly to S3 bucket
+  public async postSignatureToAws(image:any){
+    const url = await this.generateUploadUrl();
+    const httpHeaders: HttpHeaders = new HttpHeaders();
+    httpHeaders.set('content-type','mutlipart-data');
+    return this.http.put(    url,image,   {    headers: httpHeaders    }  );
   }
 }
